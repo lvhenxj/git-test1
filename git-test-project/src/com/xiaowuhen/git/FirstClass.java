188be712674cf716bd00b6d9c317cf8118dc0fe0
xiaojia <xiaojia@lvhen.cn>
2019-03-22 22:29:23 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/xiaowuhen/git/FirstClass.java
@@ -0,0 +1,22 @@
+package com.xiaowuhen.git;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+
+    }
+
+    public void test(){
+        int i = 0;
+        i += 10;
+    }
+
+
+}
